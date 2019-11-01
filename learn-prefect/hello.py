2c4c05033c6f1868cb70a4dbf7b7a3336741d363
Andrew Vaccaro <atvaccaro@gmail.com>
2019-10-31 21:39:03 -0600

    initial prefect examples

---
+++ learn-prefect/hello.py
@@ -0,0 +1,57 @@
+from prefect import task, Task, Flow, Parameter
+
+
+@task
+def say_hello():
+    print("Hello, world!")
+
+
+@task
+def add(x, y=1):
+    return x + y
+
+
+@task
+def say_hello(person: str) -> None:
+    print("Hello, {}!".format(person))
+
+
+class AddTask(Task):
+
+    def __init__(self, default: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default = default
+
+    def run(self, x: int, y: int = None) -> int:
+        if y is None:
+            y = self.default
+        return x + y
+
+
+# initialize the task instance
+addtask = AddTask(default=1)
+
+with Flow("My first flow!") as flow:
+    first_result = add(1, y=2)
+    second_result = add(x=first_result, y=100)
+
+state = flow.run()
+
+assert state.is_successful()
+
+first_task_state = state.result[first_result]
+assert first_task_state.is_successful()
+assert first_task_state.result == 3
+
+second_task_state = state.result[second_result]
+assert second_task_state.is_successful()
+assert second_task_state.result == 103
+
+with Flow("Say hi!") as flow:
+    name = Parameter("name")
+    say_hello(name)
+
+if __name__ == '__main__':
+    flow.run(name="Marvin")
+
+
---
+++ learn-prefect/imperative.py
@@ -0,0 +1,25 @@
+from prefect import task, Task, Flow, Parameter
+
+from hello import add, say_hello
+
+flow = Flow("My imperative flow!")
+
+# define some new tasks
+name = Parameter("name")
+second_add = add.copy()
+
+# add our tasks to the flow
+flow.add_task(add)
+flow.add_task(second_add)
+flow.add_task(say_hello)
+
+# create non-data dependencies so that `say_hello` waits for `second_add` to finish.
+say_hello.set_upstream(second_add, flow=flow)
+
+# create data bindings
+add.bind(x=1, y=2, flow=flow)
+second_add.bind(x=add, y=100, flow=flow)
+say_hello.bind(person=name, flow=flow)
+
+if __name__ == '__main__':
+    flow.run(name="Marvin")
